325b850c256d0d73911b2f374ce6eb74f507ccc6
williamszk <wllmszk@gmail.com>
2021-03-20 09:04:54 -0300

    update

---
+++ 210312.R
@@ -0,0 +1,18 @@
+
+# https://towardsdatascience.com/read-this-before-you-write-your-next-sql-query-%EF%B8%8F-de6b3b2c449a
+# 1. Check basic table metadata
+# 2. Check your assumptions
+# 3. Check with someone else
+
+# 1. Check basic table metadata
+# Checar por nomes da colunas.
+# Qual o significado de cada linha?
+# Existe alguma dimensão de tempo? Ou de espaço?
+# Quais são as variáveis numéricas?
+# Quais as variáveis categóricas?
+
+# 2. Check your assumptions
+# Deixe de forma explicita suas hipóteses.
+#
+
+
