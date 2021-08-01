00bca5b2f38bedbad4a36a37a50d0ad4c7700fae
williamszk <wllmszk@gmail.com>
2021-08-01 17:52:21 -0300

    experiment with sys.argv

---
+++ python_study/sys_argv/210801_01.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    
+    print(sys.argv)
+
+    # print("This is the name of the script: ", sys.argv[0])
+    # print("Number of arguments: ", len(sys.argv))
+    # print("The arguments are: " , str(sys.argv)    )
+
+# write in the terminal
+# python3 210801_01.py 1 2 
