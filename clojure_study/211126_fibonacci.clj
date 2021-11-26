655040328164ef73de5cc529f45d004f1cda65fe
williamszk <wllmszk@gmail.com>
2021-11-26 16:11:38 -0300

    finished fibonacci in clojure

---
+++ clojure_study/211126_fibonacci.clj
@@ -0,0 +1,16 @@
+;; Build a function that calculates the Fibonacci sequence
+;; give n to the function than it returns that Fibonacci number
+
+;; /usr/local/bin/clj
+
+(defn fibonacci [n]
+  (if (= n 1) 1 
+    (if (= n 2) 1
+      (+ (fibonacci (- n 2)) (fibonacci (- n 1))))))
+
+;; n = 4, the fibonacci is 3
+;; n = 7, the fibonacci is 13
+
+(fibonacci 4)
+(fibonacci 7)
+(fibonacci 8)
