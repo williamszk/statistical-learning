78e1ed8ed7a24d421389f83090d1daf8d0174ffe
williamszk <wllmszk@gmail.com>
2021-11-28 22:10:43 -0300

    advance in clojure 26-base number challenge

--- clojure_study/211126_challenge_001_02.clj
+++ clojure_study/211126_challenge_001_02.clj
@@ -60,11 +60,14 @@
     "Z" 25,
   })
 
+;; convert a letter for example "AB" to a list: ("A" "B")
 (defn convert-twentysix-to-listchar [string-twentysix]
   (map str (seq string-twentysix)))
 
-(convert-twentysix-to-listchar my-char-number)
+;; (def my-char-number "AB")
+;; (convert-twentysix-to-listchar my-char-number)
 
+;; a function to just get the value when you supply the key
 (defn get-number-map [key] 
   (get map-translator-str key))
 
@@ -73,7 +76,7 @@
 ;; (get-number-map "A")
 ;; (map get-number-map ["C" "B"])
 
-;; define an exponetiation function
+;; define a general exponetiation function
 (defn exp [x n]
   (if (zero? n) 1
     (* x (exp x (dec n)))))
@@ -96,16 +99,38 @@
 ;;     (map exp26 [1 0])))
 
 ;; function to reverse a list: my-list
-(defn reverve-list [my-list]
+(defn reverse-list [my-list]
   (reduce conj '() my-list))
+;; I don't know how this functions works...
+;; But it works for reversing PersistentVector and PersistentList
+;; Example:
+;; (def my-list [1 2 3])
+;; (reverse-list my-list)
+;; how to verify types in clojure?
+;; (type my-list)
+;; clojure.lang.PersistentVector
+;; What is difference between PersistentVector and lists in clojure?
+;;
+;; how to create a list in clojure
+;; (list 1 2 3)
+;; (type (list 1 2 3))
+;; clojure.lang.PersistentList
+;; What is the difference between PersistentVector and PersistentList in Clojure?
+;; I think that a vector needs to have the same types of data inside it
+;; but a list doesn't necessarily.
+;;
+;; 
+(def my-list (list 1 2 3))
+(type my-list)
+(reverse-list my-list)
 
 ;; a function to make a reversed list
 ;; of a range
 (defn reverse-range [n]
-  (reverve-list (range n)))
+  (reverse-list (range n)))
 ;; (reverse-range 5)
 
-;; get the length of a list
+;; How to get the length of a list in Clojure?
 ;; (def my-number-char ["C" "B"])
 ;; (count my-number-char)
 
@@ -114,7 +139,7 @@
 ;;     (map get-number-map my-number-char)
 ;;     (map exp26 (reverse-range (count my-number-char)))))
 
-;; function to convert a list of chars into
+;; function to convert a list of chars into a list of numbers
 ;; and make the sum to find the decimal number that is equivalent
 (defn convert-charlist-decnumber [my-number-char]
   (reduce + 
@@ -152,13 +177,51 @@
 ;; this should be 676
 ;; (convert-26based-to-10based-number my-char-number)
 
-;; we still need to create the function that can conver the decimal
+;; we still need to create the function that can converts the decimal
 ;; number into a 26-based number
 ;; after doing this we can solve the problem
 
 ;; CSGHJ * CBA = FNEUZJA
 ;; (convert-26based-to-10based-number "CSGHJ")
 ;; (convert-26based-to-10based-number "CBA")
+
+;; Function that converts a 1-based number into the 26-based number
+(def my-10-based-number 676)
+;; this needs to be "BAA"
+
+;; maybe I dont need this...
+;; create a list with the powers of 26
+(map exp26 (reverse-range 3))
+;; this list is used to verify in which category the 10-based number falls
+;; if it is less than 26^2? or 26^3?
+;; 
+
+;; how to take the mathematical log in clojure?
+(Math/log 10)
+(Math/log 2.71828189)
+;; this is log of base e, where e is the mathematical constant
+;; we can change the base of the log, we can look at google to find out
+
+;; build a function that finds the log of a number in base 26
+(defn get-log-base26 [number] 
+  (/ (Math/log number) (Math/log 26)))
+;; Example 1
+;; this should give 2
+;; (get-log-base26 676)
+;; Example 2
+;; (def number 676)
+;; this should give 5
+;; (get-log-base26 11881376)
+;; Just a test
+;; (get-log-base26 456)
+
+;; The function above will be important to find the number of 26-base number 
+;; slots the number will occupy
+
+
+
+
+
 
 
 
