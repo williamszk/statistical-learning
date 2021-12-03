4b36e3803935e13a311f49d8e4f6dc151af7faf7
williamszk <wllmszk@gmail.com>
2021-12-02 21:50:51 -0300

    go update

---
+++ go_study/packagetest/file1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 1.0
+	b := 2.0
+
+	mySum := SumNumbers(a, b)
+
+	fmt.Printf("%v, %T\n", mySum, mySum)
+
+}
---
+++ go_study/packagetest/file2.go
@@ -0,0 +1,5 @@
+package packagetest
+
+func SumNumbers(a float64, b float64) float64 {
+	return (a + b)
+}
