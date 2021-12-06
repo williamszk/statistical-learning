4c778b5bca12172790a4c9133f3c9f57fecfd886
williamszk <wllmszk@gmail.com>
2021-12-06 04:07:51 -0800

    fastai notes

---
+++ fastai_course/211201_01.py
@@ -0,0 +1,31 @@
+# https://www.fast.ai/
+
+
+
+# https://www.fast.ai/2019/07/08/fastai-nlp/
+# the link for fastai's course of nlp
+# https://github.com/fastai/course-nlp
+
+
+
+# fastai book:
+# https://github.com/fastai/fastbook
+
+
+
+
+# https://github.com/fastai/fastbook/blob/master/04_mnist_basics.ipynb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
