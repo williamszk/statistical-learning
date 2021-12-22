fd46d7353d55f1472aaf21130fc7d014f5e2c9f8
williamszk <wllmszk@gmail.com>
2021-12-21 16:45:13 -0800

    go study xor gate

--- go_study/random_code/m211220_01_sum_2bit_numbers_logic_gates.go
+++ go_study/random_code/m211220_01_sum_2bit_numbers_logic_gates.go
@@ -44,6 +44,3 @@ func bitwiseSum2bits(A1 int, A0 int, B1 int, B0 int) [3]int {
 
 	return answer
 }
-
-// https://www.google.com/search?q=xor+table&rlz=1C1GCEU_pt-BRBR974BR974&tbm=isch&source=iu&ictx=1&fir=Dxmvybd5iC2ehM%252CT_ctfO_9UA3ceM%252C%252Fm%252F09j4m5%253BhD31H64jsFNuUM%252CcbOD3hyd-L8XTM%252C_%253BeWWxa6n9PwRf9M%252CFmgFJdqMo1NrkM%252C_%253BvKfz5Vcu__PmnM%252CqZNsKqkIfvQTRM%252C_%253B1xXPC_hOnLV95M%252CFmgFJdqMo1NrkM%252C_%253BiXtHvZxPHP1PdM%252CWkuTm94sNfojBM%252C_&vet=1&usg=AI4_-kQBrPuQB_JwmBjNCGfQonIUEDBY0g&sa=X&ved=2ahUKEwid-bvpkfP0AhXkm-AKHdm1DYQQ_B16BAg0EAE#imgrc=Dxmvybd5iC2ehM
-// next implement an xor gate using just and, or gates
