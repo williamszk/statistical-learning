5fc025a5df3b3259380eec207226cd96afe5a9e9
williamszk <wllmszk@gmail.com>
2022-01-03 20:05:51 -0300

    webdev course

---
+++ webdev/DrumKit01/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,11 @@
+// The event listener will listen to some event like a click
+// this is different from a program running from top to bottom
+
+var numDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+  });
+}
