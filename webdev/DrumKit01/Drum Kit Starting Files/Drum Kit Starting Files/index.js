76416e95e1ea28db1662fcfdfbce47e66cd6ce10
williamszk <wllmszk@gmail.com>
2022-01-03 23:59:01 -0300

    advanced DOM section

--- webdev/DrumKit01/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ webdev/DrumKit01/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,13 +1,63 @@
 // The event listener will listen to some event like a click
 // this is different from a program running from top to bottom
 
-var numDrumButtons = document.querySelectorAll(".drum").length;
+function makeSound(buttonPressed) {
+  switch (buttonPressed) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      console.log("We got the default case: ", buttonPressed, ", which we don't make sound.")
+      break;
+  }
+}
 
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey)
+  activeButton.classList.add("pressed")
+  setTimeout(function() {
+      activeButton.classList.remove("pressed");
+    },
+    100
+  );
+
+}
+
+var numDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    this.style.color = "white";
+    var buttonPressed = this.innerHTML;
+    makeSound(buttonPressed);
+    buttonAnimation(buttonPressed);
   });
 }
 
-// var audio = new Audio("sounds/crash.mp3");
-// audio.play();
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
