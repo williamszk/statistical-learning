6e75c2da25f0597a831c557072a39174d1f81599
williamszk <wllmszk@gmail.com>
2022-01-11 21:04:41 -0300

    cpp course

---
+++ cpp_study/m220111_02_ascii_table.cpp
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=GQp1zzTwrIg&t=3900s&ab_channel=CodeBeauty
+// American Standard Code for Information Interchange - ASCII
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (int)'a' << endl;
+    // 97
+    // in ascii 'a' is equivalent to 97
+    cout << int('A') << endl;
+    // note that we use a different way of type conversion in here
+    // 65
+    // in ascci 'A' is equivalent to 65
+}
