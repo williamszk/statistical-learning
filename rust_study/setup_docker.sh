a3c6743816a358c65cfea094fa6f561bde4ac30e
williamszk <wllmszk@gmail.com>
2022-01-22 21:49:51 -0300

    rust study

---
+++ rust_study/setup_docker.sh
@@ -0,0 +1,14 @@
+# create a docker container for rust development
+
+systemctl status docker
+
+docker ps -a
+
+docker run --name rust_study -it ubuntu /bin/bash
+
+# now we are inside the docker rust_study
+
+# the next step is to use vscode inside the docker container
+
+
+
