aae8a8db9cb1553c24dbd16a947bfca00a33e497
williamszk <wllmszk@gmail.com>
2022-01-22 22:08:50 -0300

    rust shell docker

--- rust_study/setup_docker.sh
+++ rust_study/setup_docker.sh
@@ -10,5 +10,32 @@ docker run --name rust_study -it ubuntu /bin/bash
 
 # the next step is to use vscode inside the docker container
 
+# docker ps -a
+
+# docker exec -it rust_study /bin/bash
+
+apt update
+apt install git -y
+
+cd ~
+
+git clone https://github.com/williamszk/statistical-learning.git
+
+
+# I had a problem with vscode not connecting with the container
+# I just destroyed and created it again
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
