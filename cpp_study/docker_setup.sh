15b9e0b56b6603f05a311da6d03596756b1fe6a3
williamszk <wllmszk@gmail.com>
2022-01-26 21:21:36 -0300

    starting with cpp docker container

---
+++ cpp_study/docker_setup.sh
@@ -0,0 +1,37 @@
+
+# this is a docker setup script
+# I want to create a docker container to run cpp
+# with nvim
+# the setup would almost the same
+# I would need to install nvim and some configuration files for vim in there
+
+
+
+docker run --name cpp_study -it ubuntu /bin/bash
+apt update
+apt install git -y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ webdev/intro-to-node/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+
+// fs.copyFileSync("./file1.txt", "file1-copy.txt");
+
+
+let mySuperHero = superheroes.random();
+let mySuperVillain = supervillains.random();
+
+console.log("mySuperHero:", mySuperHero);
+console.log("mySuperVillain:", mySuperVillain);
+
+// to initialize a new package in node
+// npm init
+
+// this will create a file package.json
+// which will keep track of the packages that we use in this particular project
+// npm install superheroes
+// npm install supervillains
