4e404aa08f507182622dff22d16b858e82990ad2
williamszk <wllmszk@gmail.com>
2022-01-29 02:34:06 +0000

    cpp study

---
+++ cpp_study/u220127_03.cpp
@@ -0,0 +1,22 @@
+
+// https://www.udemy.com/course/the-modern-cpp-20-masterclass/learn/lecture/21293120#overview
+// Different types of errors
+// 1. Compile time errors
+// 2. Runtime errors
+// 3. Warnings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
