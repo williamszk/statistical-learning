cbec35a8dda33464e73df0b1f176e43bf63cfef3
williamszk <wllmszk@gmail.com>
2022-01-29 01:53:16 -0300

    update notes on R study

--- R_study/docker_setup.sh
+++ R_study/docker_setup.sh
@@ -8,3 +8,18 @@ pip3 install jupyter
 # enable jupyter extension in container
 # https://www.youtube.com/watch?v=_10avr-dOSE&t=280s&ab_channel=1littlecoder
 # take a look at the video above to setup the R with jupyter
+
+# inside R shell we need to install
+install.packages("IRkernel")
+# then we need to call the package
+IRkernel::installspec()
+
+# for the step above we need to have jupyter installed...
+# for that we need python
+# and then
+pip3 install jupyter
+# and for vscode conected to the container
+# we need to have the jupyter extension activated
+
+
+
