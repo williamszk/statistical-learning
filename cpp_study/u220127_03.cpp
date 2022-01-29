266d4a4fab10f294bd40c2dc619e61b62f54097d
williamszk <wllmszk@gmail.com>
2022-01-29 03:52:52 +0000

    cpp study

--- cpp_study/u220127_03.cpp
+++ cpp_study/u220127_03.cpp
@@ -6,7 +6,21 @@
 // 3. Warnings
 
 
+// https://www.udemy.com/course/the-modern-cpp-20-masterclass/learn/lecture/21293126?start=15#overview
 
+#include <iostream>
+
+int main()
+{
+    // this is a different way to declare variables
+    int firstNumber {3};
+    int secondNumber {7};
+
+    std::cout << "firstNumber: " << firstNumber << std::endl;
+    std::cout << "secondNumber: " << secondNumber << std::endl;
+    
+    
+}
 
 
 
