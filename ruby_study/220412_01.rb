d0804f5d6e86b2a6eed10b52c49d6c4d4f3d8c22
williamszk <wllmszk@gmail.com>
2022-04-12 19:11:11 -0300

    tinkering with ruby

---
+++ ruby_study/220412_01.rb
@@ -0,0 +1,83 @@
+# https://www.youtube.com/watch?v=t_ispmWmdjY&ab_channel=freeCodeCamp.org
+
+# some tinkering with ruby
+
+# sudo apt install ruby
+
+# in the terminal
+# irb
+
+print "Hello World"
+
+"hello world".class
+
+# how to see the type of a variable
+
+true.class
+
+false.class
+
+nil.class
+
+# https://stackoverflow.com/questions/15769739/determining-type-of-an-object-in-ruby#:~:text=The%20proper%20way%20to%20determine%20the%20%22type%22%20of%20an%20object,type%22%20you%20might%20call%20object.
+
+0.99.class
+
+10.class
+
+:animal.class
+
+# this is a symbol
+:yes
+# it is kinda a string but not
+# we can use it inside dictionaries, maps...
+
+
+# powers
+2**3
+
+# we can work with binary using 0b
+0b1010
+
+# we can work with hexadecimal too
+0xFF
+
+# converts to string
+102.to_s
+
+102.to_s.class
+
+# is class an attribute or method?
+# is to_s an attribute or method
+# not using () doesn't pose a problem?
+
+-931.abs
+
+-98.22.round
+-87.55.round
+
+98.88.ceil
+98.88.floor
+
+Math.class
+# Math is a module
+
+Math.sqrt(9)
+Math.sqrt(34)
+
+Math.log(10)
+
+# there are Float and Integer in ruby
+
+10 / 7
+10 / 7.0
+
+# stoped here
+# https://youtu.be/t_ispmWmdjY?t=3240
+
+
+
+
+
+
+
