bfa4bad88b63ea7b2e0d8b6367536075786fe3d2
williamszk <wllmszk@gmail.com>
2022-04-18 10:48:00 -0700

    C study

---
+++ cpp_study/C_course/220414_01_ascii/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    char char1 = 'E';
+    printf("%c\t%d\n", char1, char1);
+
+    char char2 = 'P';
+    printf("%c\t%d\n", char2, char2);
+
+    char char3 = 'Y';
+    printf("%c\t%d\n", char3, char3);
+}
