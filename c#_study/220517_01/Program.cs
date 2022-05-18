414e9306523505e41f47561502bca2997a39d1d2
williamszk <wllmszk@gmail.com>
2022-05-17 23:58:38 -0300

    C# tinkering

---
+++ c#_study/220517_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var name = System.Environment.MachineName;
+Console.WriteLine("Hello " + name);
+
+
+// https://www.youtube.com/watch?v=Z5JS36NlJiU&ab_channel=dotNET
+
+string aFriend = "Bill";
+Console.WriteLine(aFriend);
+
+// this is string interpolation it is like in python
+// instead of f we use $
+// in javascript we need to use `` for string literals
+// and use $ in the beginning of {}
+Console.WriteLine($"Hello {aFriend}");
+
+// stoped at:
+// https://youtu.be/Z5JS36NlJiU?t=334
