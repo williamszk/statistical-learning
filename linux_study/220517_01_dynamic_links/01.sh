dbff3ef773cd421d8e67064c94d3ee331ee2cb11
williamszk <wllmszk@gmail.com>
2022-05-18 22:14:50 -0300

    understand .o .a ar using C

---
+++ linux_study/220517_01_dynamic_links/01.sh
@@ -0,0 +1,20 @@
+# https://opensource.com/article/20/6/linux-libraries
+
+git clone https://github.com/hANSIc99/library_sample
+
+cd library_sample/
+
+make clean
+
+make
+
+make clean
+
+
+
+
+
+
+
+
+
