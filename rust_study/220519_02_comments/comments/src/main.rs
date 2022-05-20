be87aeb597e8d8e2efdea35ef929392825e31b3b
williamszk <wllmszk@gmail.com>
2022-05-20 03:08:27 +0000

    rust playing

---
+++ rust_study/220519_02_comments/comments/src/main.rs
@@ -0,0 +1,16 @@
+/// This project is used to explore comments in rust
+
+fn main() {
+    //! # Main thread
+    //! 
+    //! fn main()
+    //! 
+    //! Reads user input and returns it.
+    //! 
+    //! ```
+    //! fn()  main {
+    //!     
+    //! }
+    //! ```
+    println!("Hello, world!");
+}
