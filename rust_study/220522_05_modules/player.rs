8b3843215abc9c22bf06d5fd5c828a2edf2e808a
williamszk <wllmszk@gmail.com>
2022-05-24 00:33:00 +0000

    rust study

---
+++ rust_study/220522_05_modules/player.rs
@@ -0,0 +1,9 @@
+// https://www.udemy.com/course/rustaceans/learn/lecture/19478246?start=15#overview
+
+pub fn play_movies(name: &str) {
+    println!("Playing movie {}", name);
+}
+
+pub fn play_audio(name: &str){
+    println!("Playing audio {}", name);
+}
