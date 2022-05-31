ba7fe075f7faa3a8cc72155568027e36ccbfc447
williamszk <wllmszk@gmail.com>
2022-05-31 00:52:06 -0300

    java functional programming

---
+++ java_study/220528_01_functional/Main.java
@@ -0,0 +1,7 @@
+// https://www.youtube.com/watch?v=VRpHdSFWGPs&list=RDCMUC2KfmYEM4KCuA1ZurravgYw&index=3&ab_channel=Amigoscode
+
+
+
+
+
+
---
+++ java_study/220528_01_functional/readme.md
@@ -0,0 +1,6 @@
+Those are some notes about functional programming with Java:
+
+
+last checkpoint:
+https://youtu.be/VRpHdSFWGPs?t=2165
+
