52c560a400fb2ab4a3ae3db1d9d63c95389d93c1
williamszk <wllmszk@gmail.com>
2022-06-02 23:23:49 -0300

    django study

---
+++ python_study/220602_01_django_ivy/setup.sh
@@ -0,0 +1,18 @@
+
+# https://www.udemy.com/course/python-django-2021-complete-course/learn/lecture/27268008#overview 
+
+
+cd 220602_01_django_ivy
+django-admin startproject devsearch
+
+cd devsearch
+
+# to run the server in dev mode
+python3 manage.py runserver
+
+
+# to create an app in the project
+python3 manage.py startapp projects
+# projects is also the name of the app
+
+
