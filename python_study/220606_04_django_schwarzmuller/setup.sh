8a058236fc10b2b17d845e0c326cb16e330e301e
williamszk <wllmszk@gmail.com>
2022-06-07 01:06:03 -0300

    django udemy schwarmuller

---
+++ python_study/220606_04_django_schwarzmuller/setup.sh
@@ -0,0 +1,12 @@
+
+# https://www.udemy.com/course/python-django-the-practical-guide/learn/lecture/26372804?start=15#overview 
+
+django-admin startproject monthly_challenges
+
+cd monthly_challenges
+
+python3 manage.py startapp challenges 
+
+# start the application
+python3 manage.py runserver 
+
