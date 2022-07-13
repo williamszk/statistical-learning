936f0a8a8f734033cf0859413721027444df86d5
williamszk <wllmszk@gmail.com>
2022-07-12 19:18:25 -0700

    scala study

---
+++ scala_study/220712_01/main.scala
@@ -0,0 +1,22 @@
+// https://www.udemy.com/course/rock-the-jvm-scala-for-beginners/learn/lecture/10373000#overview
+
+// https://github.com/rockthejvm/udemy-scala-beginners
+
+/*
+set PATH=%PATH%;C:\Users\william.suzuki\null\Coursier\data\bin
+scala3-repl.bat
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ scala_study/220712_01/scala/playground/Playground.scala
@@ -0,0 +1,13 @@
+// package playground
+/*
+scalac scala\playground\Playground.scala
+scala Playground
+ */
+
+object Playground {
+
+  def main(args: Array[String]): Unit = {
+    println("I am ready to learn Scala!")
+
+  }
+}
