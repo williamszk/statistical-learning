f82246b164a4fc69fa4e04bfa67950dcde1acb5b
williamszk <wllmszk@gmail.com>
2022-07-16 14:21:59 -0300

    javascript the good parts and some regex

---
+++ javascript_study/220716_01_javascript_the_good_parts/readme.md
@@ -0,0 +1,8 @@
+
+I am reading: JavaScript the Good Parts by Douglas Crockford.
+
+Here I write some of the notes based on the book.
+
+
+Stopped at chapter 3: Objects
+
---
+++ javascript_study/220716_01_javascript_the_good_parts/study01/program.js
@@ -0,0 +1,20 @@
+document.writeln('Hello, World!')
+
+document.writeln(2e10) // 20000000000
+document.writeln(2e5) // 200000
+
+NaN
+document.writeln(typeof(NaN)) // number
+document.writeln(isNaN(NaN)) // true
+document.writeln(NaN == NaN) // false
+
+document.writeln(typeof [1,2,3]) // object, this is wrong
+document.writeln(typeof null) // object, this is wrong
+
+
+
+
+
+
+
+
