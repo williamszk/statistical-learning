a512a80957d7ac5a537f4ae7a09dcc5e2f4a7bf9
williamszk <wllmszk@gmail.com>
2022-08-01 20:09:24 -0700

    java study

---
+++ java_study/220801_01_amigoscode/src/com/amigoscode/Main.java
@@ -0,0 +1,10 @@
+package com.amigoscode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Main inside com.amigoscode");
+    }
+}
+
+// java com.amigoscode.Main
