c20bb6ca76968122b0566b7f4fe7f40484bdf26f
williamszk <wllmszk@gmail.com>
2022-08-07 12:28:41 +0000

    rust study

--- rust_study/220803_01_banas/rust_tutorial/src/main.rs
+++ rust_study/220803_01_banas/rust_tutorial/src/main.rs
@@ -40,7 +40,48 @@ fn main() {
 
     let random_num = rand::thread_rng().gen_range(1..101);
     println!("Random: {}", random_num);
+
+    age = 8;
+    if (age >= 1) && (age <= 18) {
+        println!("You have an important birthday");
+    } else if (age == 21) || (age == 50) {
+        println!("You have an important birthday");
+    } else if age >= 65 {
+        println!("You have an important birthday");
+    } else {
+        println!("You have an NOT important birthday");
+    }
+
+    // ternary operator
+    let mut my_age = 47;
+    let can_vote = if my_age >= 18 { true } else { false };
+
+    println!("Can vote: {}", can_vote);
+
+    // match, it is like switch
+    let age2 = 19;
+    match age2 {
+        // this will go from 1 to 18 included
+        1..=18 => println!("You have an important birthday"),
+        21 | 51 => println!("You have an important birthday"),
+        65..=i32::MAX => println!("You have an important birthday"),
+        _ => println!("You have an NOT important birthday"),
+    };
+
+    // match is an statement it does not return anything
+
+    // another possibility that we can work with
+    let my_age = 18;
+    let voting_age = 18;
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't vote"),
+        Ordering::Greater => println!("Can vote"),
+        Ordering::Equal => println!("You gained the right to vote"),
+    };
+
+
 }
 
-// Stoped at:
 // https://youtu.be/ygL_xcavzQ4?t=1452
+
+// Stoped at:
