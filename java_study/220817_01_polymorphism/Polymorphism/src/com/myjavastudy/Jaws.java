53de4d193cb6373ff1a3e630e9ab3d717bd98c9d
williamszk <wllmszk@gmail.com>
2022-08-17 18:33:21 -0700

    java polymorphism study

---
+++ java_study/220817_01_polymorphism/Polymorphism/src/com/myjavastudy/Jaws.java
@@ -0,0 +1,14 @@
+package com.myjavastudy;
+
+public class Jaws extends Movie {
+
+    public Jaws() {
+        super("Jaws");
+    }
+
+    @Override
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+
+}
