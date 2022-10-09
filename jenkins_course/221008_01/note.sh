8b228b89bfc9126e0c943d1576781020900f987b
williamszk <wllmszk@gmail.com>
2022-10-09 20:52:31 -0300

    jenkins study

--- jenkins_course/221008_01/note.sh
+++ jenkins_course/221008_01/note.sh
@@ -41,6 +41,49 @@ docker logs -f jenkins
 # https://youtu.be/nCKxl7Q_20I?t=417
 
 
-# -------------------
+# ----------------------------------------------------
 # stopped at:
 # https://youtu.be/nCKxl7Q_20I?t=1927
+# The video will enter in the part he works with Maven and Jenkins
+
+# ----------------------------------------------------
+# FullCycle Jenkins video
+# https://www.youtube.com/watch?v=8OfhS5f7jIY&ab_channel=FullCycle
+
+# just a checkpoint
+# https://youtu.be/8OfhS5f7jIY?t=2177
+# I had a problem with not having php and composer inside the container
+
+# I need to install PHP inside the Jenkis server
+# and then install Composer
+
+# https://computingforgeeks.com/how-to-install-latest-php-on-debian/
+
+# Step 1: Update system 
+apt update && apt upgrade -y && reboot
+
+# Step 2: Add SURY PHP PPA repository
+apt -y install lsb-release apt-transport-https ca-certificates 
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+
+# Step 3: Install PHP 7.4 on Debian 10 / Debian 9
+apt update
+apt install php7.4 -y
+
+# How to install Composer?
+# https://www.hostinger.com/tutorials/how-to-install-composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+# I have a problem with the container the default user is not root
+# https://stackoverflow.com/a/72042637/15875971
+docker exec -u 0 -it jenkins /bin/bash
+# this works!
+# we just need to include -u 0 
+
+# try to run the job again
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/bin/composer
+# ----------------------------------------------------
+
