55df4c1ec912b93ca48b2afff733e56c636b0f90
williamszk <wllmszk@gmail.com>
2022-10-22 23:52:37 -0300

    django study

---
+++ django_study/udemy-dennis-ivy/221022_01/notes.sh
@@ -0,0 +1,13 @@
+# https://www.udemy.com/course/python-django-2021-complete-course/learn/lecture/27268008#overview
+
+pip install Django
+pip freeze
+django-admin
+
+django-admin startproject devsearch
+cd devsearch
+python3 manage.py runserver
+
+
+python3 manage.py startapp projects 
+cd projects
