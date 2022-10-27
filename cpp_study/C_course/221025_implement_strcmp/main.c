bf8139bef8f56941f05c50cb4477f44f30220e49
williamszk <wllmszk@gmail.com>
2022-10-26 23:29:18 -0300

    C study

--- cpp_study/C_course/221025_implement_strcmp/main.c
+++ cpp_study/C_course/221025_implement_strcmp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 // https://www.udemy.com/course/c-programming-for-beginners-programming-in-c/learn/lecture/24168704#questions
 
@@ -15,35 +16,61 @@
  * @param str2
  * @return int
  */
-// int strcmpUser(char *str1, char *str2)
-// {
-//     int i;
-//     int lenStr1 = sizeof(str1) / sizeof(char);
-//     int lenStr2 = sizeof(str2) / sizeof(char);
+int strcmpUser(char *str1, char *str2)
+{
+    int i;
+    int lenStr1 = sizeof(str1) / sizeof(char);
+    int lenStr2 = sizeof(str2) / sizeof(char);
 
-//     // printf("lenStr1 = %d\n", lenStr1);
-//     // printf("lenStr2 = %d\n", lenStr2);
+    // printf("lenStr1 = %d\n", lenStr1);
+    // printf("lenStr2 = %d\n", lenStr2);
 
-//     for (i = 0; i < lenStr1; i++)
-//     {
-//         if (str1[i] > str2[i])
-//         {
-//             return 1;
-//         }
-//         else
-//         {
-//             return -1;
-//         }
-//     }
+    for (i = 0; i < lenStr1; i++)
+    {
+        if (str1[i] > str2[i])
+        {
+            return 1;
+        }
+        else if (str1[i] < str2[i])
+        {
+            return -1;
+        }
+    }
 
-//     return 0;
-// }
+    return 0;
+}
+
+int strcmpUser2(char *str1, char *str2)
+{
+    int i = 0;
+    int flag = 1;
+
+    while (true)
+    {
+        if (str1[i] > str2[i])
+            flag = 0;
+        else if (str1[i] > str2[i])
+            flag = 1;
+        else if (str1[i] < str2[i])
+            flag = -1;
+        else if (str1[i] == '\0' || str2[i] == '\0')
+            break;
+        
+        i++;
+    }
+
+    return flag;
+}
 
 int main()
 {
-    // char str1[100] = "Banana";
-    // char str2[100] = "Banana";
+    char str1[100] = "Banana";
+    char str2[100] = "Banana";
 
-    // printf("01 is equal? %d\n", strcmpUser(str1, str2));
-    printf("Hello World\n");
+    printf("01 strcmpUser is equal? %d it should be 0\n", strcmpUser(str1, str2));
+
+    strcpy(str1, "ab");
+    strcpy(str2, "ab");
+    printf("02 strcmpUser is equal? %d it should be -1\n", strcmpUser(str1, str2));
+    printf("02 strcmpUser2 is equal? %d it should be -1\n", strcmpUser2(str1, str2));
 }
