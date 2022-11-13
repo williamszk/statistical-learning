0596966439de99a53a1acc026f2cd4656f322d27
williamszk <wllmszk@gmail.com>
2022-11-12 23:37:56 -0300

    docker react app study

---
+++ docker_kubernetes_study/udemy-docker-kubernetes-grider/221112/notes.sh
@@ -0,0 +1,22 @@
+
+https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/learn/lecture/11437044#overview
+
+# check if node is installed
+node -v
+# v16.17.0
+
+npm init -y
+npx create-react-app frontend
+
+npm run start # development server
+npm run test  # running tests
+npm run build # for deployment
+
+
+
+
+
+
+
+
+
