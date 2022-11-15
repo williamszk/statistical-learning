e3bf388d37d35c3f9abe0f67c6ef1a79ae2b598b
williamszk <wllmszk@gmail.com>
2022-11-15 01:34:04 +0000

    buchana article about compressed ecc representation

---
+++ cryptography_study/buchanan-material/221114_01/notes.sh
@@ -0,0 +1,8 @@
+# Notes based on:
+# https://medium.com/asecuritysite-when-bob-met-alice/02-03-or-04-so-what-are-compressed-and-uncompressed-public-keys-6abcb57efeb6
+
+npm init -y
+
+# https://www.npmjs.com/package/eth-crypto
+npm i eth-crypto
+
