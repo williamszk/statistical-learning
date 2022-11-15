f4b72bc57f84e895bef76f5bd17e22c973809d2a
williamszk <wllmszk@gmail.com>
2022-11-15 02:38:59 +0000

    python sha keccak shake buchanan

---
+++ cryptography_study/buchanan-material/221114_02/notes.sh
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=bTOJ9An9wpE&ab_channel=BillBuchananOBE
+
+# https://asecuritysite.com/hash/s3
+
+python3 -m venv .venv
+
+# Still need to take a look at:
+# https://asecuritysite.com/hash/goshake
+# https://asecuritysite.com/hash/gokang
+
