d95cf22d6df8fa8f9d55d1ecb91632a7e32d8361
williamszk <wllmszk@gmail.com>
2022-11-25 16:19:49 +0000

    machine learning study, handson geron

--- machine_learning_study/book-handson-geron/notes.sh
+++ machine_learning_study/book-handson-geron/notes.sh
@@ -8,8 +8,8 @@ pip3 install jupyterlab==3.6.0a4
 pip3 install jupyterlab-vim
 
 # to start the jupyter notebook
-# jupyter notebook --ip 0.0.0.0 --allow-root --port 3000 --no-browse
-jupyter lab --ip 0.0.0.0 --allow-root --port 3000 --no-browse
+# jupyter notebook --ip 0.0.0.0 --allow-root --port 3000 --no-browser
+jupyter lab --ip 0.0.0.0 --allow-root --port 3000 --no-browser
 # https://github.com/jwkvam/jupyterlab-vim
 
 hello hello
