fc888e6e099972a4d86d4463ee226e4657ad55e0
williamszk <wllmszk@gmail.com>
2022-11-29 20:46:47 +0000

    templating study django

--- django_study/udemy-dennis-ivy/221129_01/devsearch/projects/views.py
+++ django_study/udemy-dennis-ivy/221129_01/devsearch/projects/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+projects_list = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 
 
 def projects(request):
-    # return HttpResponse("Here are some products.")
-    return render(request, "projects/projects.html")
+    page = "Projects"
+    number = 10
+    context = {
+        "page": page,
+        "number": number,
+        "projects": projects_list,
+    }
+    return render(request, "projects/projects.html", context)
 
 def project(request, pk):
-    # return HttpResponse(f"Here is your specific project. With id {pk}.")
-    return render(request, "projects/single-project.html")
+    chosen_project = None
+    for project_item in projects_list:
+        if project_item["id"] == pk:
+            chosen_project = project_item
+            break
+
+    context = {
+        "project": chosen_project,
+    }
+    return render(request, "projects/single-project.html", context)
