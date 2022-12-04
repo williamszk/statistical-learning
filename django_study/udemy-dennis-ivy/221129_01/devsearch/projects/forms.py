4b323f91580cec6912ff163d06da70557b162c87
williamszk <wllmszk@gmail.com>
2022-12-04 19:43:23 +0000

    django study app "projects"

--- django_study/udemy-dennis-ivy/221129_01/devsearch/projects/forms.py
+++ django_study/udemy-dennis-ivy/221129_01/devsearch/projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -14,3 +15,18 @@ class ProjectForm(ModelForm):
             "source_link",
             "tags",
         ]
+        
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields["title"].widget.attrs.update({"class":"input", "placeholder": "Add Title"})
+        # self.fields["description"].widget.attrs.update({"class":"input"})
+        # self.fields["featured_image"].widget.attrs.update({"class":"input"})
+        # self.fields["demo_link"].widget.attrs.update({"class":"input"})
+        # self.fields["source_link"].widget.attrs.update({"class":"input"})
+        for name, field in self.fields.items():
+            self.fields[name].widget.attrs.update({"class":"input"})
