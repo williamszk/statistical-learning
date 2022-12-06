640338a1b2c2acdcb7a113a0c23c1e69aedbbdac
williamszk <wllmszk@gmail.com>
2022-12-06 16:16:05 +0000

    django study

--- django_study/udemy-dennis-ivy/221129_01/devsearch/users/views.py
+++ django_study/udemy-dennis-ivy/221129_01/devsearch/users/views.py
@@ -11,7 +11,12 @@ def profiles(request):
 def user_profile(request, pk):
     profile = Profile.objects.get(id=pk)
 
-    top_skills = 
+    top_skills = profile.skill_set.exclude(description__exact="")
+    other_skills = profile.skill_set.filter(description="")
 
-    context = {"profile": profile}
+    context = {
+        "profile": profile,
+        "top_skills": top_skills,
+        "other_skills": other_skills,
+    }
     return render(request, "users/user-profile.html", context)
