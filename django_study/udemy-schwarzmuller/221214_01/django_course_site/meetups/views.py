3b690a4b7b80e546154762627d716c94aa9a44d7
williamszk <wllmszk@gmail.com>
2022-12-19 07:24:16 -0800

    django study schwarzmuller

--- django_study/udemy-schwarzmuller/221214_01/django_course_site/meetups/views.py
+++ django_study/udemy-schwarzmuller/221214_01/django_course_site/meetups/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<p>Hi there</p>") 
+    # return HttpResponse("<p>Hi there</p>") 
+    meetups = [
+        {"title":"A first meetup"},
+        {"title":"A Second meetup"},
+    ]
+    context = {
+        "show_meetups": True,
+        "meetups": meetups,
+    }
+    return render(request, "meetups/index.html", context)
--- django_study/udemy-schwarzmuller/readme.md
+++ django_study/udemy-schwarzmuller/readme.md
@@ -2,3 +2,5 @@ Course based on:
 
 https://www.udemy.com/course/python-django-the-practical-guide
 
+Stopped at:
+https://www.udemy.com/course/python-django-the-practical-guide/learn/lecture/26399530#overview
