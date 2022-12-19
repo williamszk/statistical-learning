50b21f6f026071472fa0af9ff15f07e6a7062323
williamszk <wllmszk@gmail.com>
2022-12-19 13:09:23 -0800

    reform python_study directory

---
+++ python_study/book-effective-python/221219_01/01.py
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+squares = []
+for x in a:
+    squares.append(x**2)
+print(squares)
+
+
+
+
---
+++ python_study/pytorch_study/readme.md
@@ -0,0 +1,2 @@
+In this directory we include files that are related to the study of the
+library PyTorch
