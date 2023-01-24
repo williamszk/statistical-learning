b4a8fd4a3dd6a2d9b59196dbc29c33668f39437d
williamszk <wllmszk@gmail.com>
2023-01-24 15:14:15 -0800

    spark study

--- pyspark_study/youtube-freecodecamp-krishna/230123_01/notes.sh
+++ pyspark_study/youtube-freecodecamp-krishna/230123_01/notes.sh
@@ -1,8 +1,11 @@
-
+pip install wheel
 pip install pyspark
+pip install pandas 
 
 
 curl -LO https://raw.githubusercontent.com/krishnaik06/Pyspark-With-Python/main/test1.csv
+curl -LO https://raw.githubusercontent.com/krishnaik06/Pyspark-With-Python/main/tips.csv
 
 # Stopped at:
 # https://youtu.be/_C8kWso4ne4?t=524
+# https://youtu.be/_C8kWso4ne4?t=920
