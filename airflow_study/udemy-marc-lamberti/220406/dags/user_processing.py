3c3237f1b4c3582a3851042eedf1011c12160194
williamszk <wllmszk@gmail.com>
2023-04-06 23:44:23 -0300

    airflow study, marc lamberti udemy

---
+++ airflow_study/udemy-marc-lamberti/220406/dags/user_processing.py
@@ -0,0 +1,28 @@
+from airflow import DAG 
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
+
+from datetime import datetime
+
+with DAG(dag_id="user_processing", start_date=datetime(2023,4,1), schedule_interval="@daily", catchup=False ) as dag:
+
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres",
+        sql="""--sql
+        CREATE TABLE IF NOT EXISTS users(
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL
+        );
+        """
+    )
+
+    is_api_available = HttpSensor(
+        task_id="is_api_available",
+        http_conn_id="user_api",
+        endpoint="api/"
+        )
