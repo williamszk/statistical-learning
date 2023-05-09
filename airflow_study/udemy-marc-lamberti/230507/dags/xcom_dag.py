22478fbeb64ff73f4a448991d49f5050eb61dd0e
williamszk <wllmszk@gmail.com>
2023-05-09 00:52:22 +0000

    airflow study, lamberti

--- airflow_study/udemy-marc-lamberti/230507/dags/xcom_dag.py
+++ airflow_study/udemy-marc-lamberti/230507/dags/xcom_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
 
 from datetime import datetime
@@ -10,12 +10,23 @@ def _t1(ti):
 def _t2(ti):
     ti.xcom_pull(key='my_key', task_ids='t1')
 
+def _branch(ti):
+    value = ti.xcom_pull(key="my_key", task_ids="t1")
+    if value == 42:
+        return "t2"
+    return "t3"
+
 with DAG("xcom_dag", start_date=datetime(2023,1,1), 
          schedule_interval="@daily", catchup=False) as dag:
     
     t1 = PythonOperator(
         task_id="t1",
         python_callable=_t1
+    )
+
+    branch = BranchPythonOperator(
+        task_id="branch",
+        python_callable=_branch
     )
 
     t2 = PythonOperator(
@@ -28,7 +39,13 @@ with DAG("xcom_dag", start_date=datetime(2023,1,1),
         bash_command="echo ''"
     )
 
-    t1 >> t2 >> t3
+    t4 = BashOperator(
+        task_id="t4",
+        bash_command="echo ''",
+        trigger_rule="none_failed_min_one_success"
+    )
+
+    t1 >> branch >> [t2 ,t3] >> t4
 
 
 
