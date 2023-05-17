b37e4c0ebde75d4aef4b54a51da9f623b5bf8072
williamszk <wllmszk@gmail.com>
2023-05-17 23:56:55 +0000

    airflow study

--- airflow_study/udemy-marc-lamberti/230515/mnt/airflow/dags/forex_data_pipeline.py
+++ airflow_study/udemy-marc-lamberti/230515/mnt/airflow/dags/forex_data_pipeline.py
@@ -1,7 +1,15 @@
+import csv
+import json
+import requests
+from datetime import datetime, timedelta
+
 from airflow import DAG
 from airflow.providers.http.sensors.http import HttpSensor
-from datetime import datetime, timedelta
 from airflow.sensors.filesystem import FileSensor
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.apache.hive.operators.hive import HiveOperator
+from airflow.providers.apache.spark.operators import spark_submit
 
 default_args = {
     "owner": "airflow",
@@ -11,6 +19,27 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
+
+
+def download_rates():
+    BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
+    ENDPOINTS = {
+        "USD": "api_forex_exchange_usd.json",
+        "EUR": "api_forex_exchange_eur.json",
+    }
+    with open("/opt/airflow/dags/files/forex_currencies.csv") as forex_currencies:
+        reader = csv.DictReader(forex_currencies, delimiter=";")
+        for idx, row in enumerate(reader):
+            base = row["base"]  # EUR, USD
+            with_pairs = row["with_pairs"].split(" ")  # USD NZD JPY GBP CAD
+            in_data = requests.get(f"{BASE_URL}{ENDPOINTS[base]}").json()
+            out_data = {"base": base, "rates": {}, "last_update": in_data["date"]}
+            for pair in with_pairs:
+                out_data["rates"][pair] = in_data["rates"][pair]
+            with open("/opt/airflow/dags/files/forex_rates.json", "a") as outfile:
+                json.dump(out_data, outfile)
+                outfile.write("\n")
+
 
 with DAG(
     "forex_data_pipeline",
@@ -38,4 +67,43 @@ with DAG(
         filepath="forex_currencies.csv",
         poke_interval=5,
         timeout=20,
+    )
+
+    downloading_rates = PythonOperator(
+        task_id="downloading_rates",
+        python_callable=download_rates,
+    )
+
+    saving_rates = BashOperator(
+        task_id="saving_rates",
+        bash_command="""
+            hdfs dfs -mkdir -p /forex && \
+            hdfs dfs -put -f $AIRFLOW_HOME/dags/files/forex_rates.json /forex
+        """,
+    )
+
+    # for the connection
+    # Conn Type: Hive Server 2 Thrift
+    # Host: hive-server
+    # Login: hive
+    # Password: hive
+    # Port: 10000
+    creating_forex_rates_table = HiveOperator(
+        task_id="creating_forex_rates_table",
+        hive_cli_conn_id="hive_conn",
+        hql="""
+            CREATE EXTERNAL TABLE IF NOT EXISTS forex_rates(
+                base STRING,
+                last_update DATE,
+                eur DOUBLE,
+                usd DOUBLE,
+                nzd DOUBLE,
+                gbp DOUBLE,
+                jpy DOUBLE,
+                cad DOUBLE
+                )
+            ROW FORMAT DELIMITED
+            FIELDS TERMINATED BY ','
+            STORED AS TEXTFILE
+        """
     )
--- airflow_study/udemy-marc-lamberti/230515/notes.sh
+++ airflow_study/udemy-marc-lamberti/230515/notes.sh
@@ -3,7 +3,15 @@ python3 -m pip install --upgrade pip
 pip install apache-airflow
 pip install 'apache-airflow[postgres]'
 pip install pandas
+pip install requests
+python3 -m pip install types-requests
 
+# for hive
+sudo apt-get install libsasl2-dev
+pip install apache-airflow-providers-apache-hive
+
+# for spark
+pip install apache-airflow-providers-apache-spark
 
 ./start.sh
 
@@ -24,4 +32,31 @@ airflow tasks list forex_data_pipeline
 # test the new task by running it 
 airflow tasks test forex_data_pipeline is_forex_available 2023-01-01
 airflow tasks test forex_data_pipeline forex_file_sensor 2023-01-01
+
+# test the new task downloading_rates
+docker exec -it e6f5e935cc76 bash
+airflow tasks test forex_data_pipeline downloading_rates 2023-01-01
+# check the output of the task
+cat /opt/airflow/dags/files/forex_rates.json
+
+# test the new task "saving_rates"
+docker exec -it 2b7821eb3861 bash
+airflow tasks list forex_data_pipeline
+airflow tasks test forex_data_pipeline saving_rates 2023-01-01
+
+# test the new task "creating_forex_rates_table"
+docker exec -it 2b7821eb3861 bash
+airflow tasks list forex_data_pipeline
+airflow tasks test forex_data_pipeline creating_forex_rates_table 2023-01-01
+
+
+
+
+
+
+
+
+
+
+
 
