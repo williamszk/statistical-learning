5b41773cef17df0dfe101c8f88348d8585e46b20
williamszk <wllmszk@gmail.com>
2023-06-18 19:02:21 +0000

    haskell study

---
+++ haskell_study/udemy-learning-path-haskell/230615/02_first_program/notes.sh
@@ -0,0 +1,16 @@
+
+touch stack.yaml
+
+# before running this command we need the stack.yaml
+stack runghc Program.hs
+# this is an interpreter
+# but the compiler will also 
+
+# this is the compiler
+# this will produce an executable
+stack ghc Program.hs
+./Program
+
+
+
+
